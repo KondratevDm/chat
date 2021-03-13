593a2d866d7ec69190336bea809490ba0c62f4c8
DmitriyKondratev1 <conddd@yandex.ru>
2021-03-13 15:00:53 +0300

    added form validation

--- client/components/login.js
+++ client/components/login.js
@@ -1,56 +1,86 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
 import { updateEmailField, updatePasswordField, loginFunction } from '../redux/reducers/auth'
 
 import Head from './head'
 
 const Login = () => {
   const dispatch = useDispatch()
-  const email = useSelector((s) => s.auth.email)
-  const password = useSelector((s) => s.auth.password)
+  // const email = useSelector((s) => s.auth.email)
+  // const password = useSelector((s) => s.auth.password)
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email('Invalid email format').required('Required!'),
+      password: Yup.string().min(6, 'Minimum 6 characters').required('Required!'),
+    }),
+    onSubmit: (values) => {
+      dispatch(updateEmailField(values.email))
+      dispatch(updatePasswordField(values.password))
+      dispatch(loginFunction())
+    }
+  })
 
   return (
     <div className="h-screen w-screen">
       <Head title="Login" />
       <div className="w-5/6 sm:w-3/5 md:w-2/4 lg:w-2/5 xl:w-1/4 2xl:w-1/4 px-6 flex justify-center flex-col  h-full mx-auto ">
-        <p className="font-medium text-center text-2xl mb-5 text-black">Login a profile</p>
-        <div className="py-2">
-          <span className="px-1 text-sm text-gray-600">Email</span>
-          <input
-            placeholder=""
-            type="email"
-            value={email}
-            x-model="password_confirm"
-            className="text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none"
-            onChange={(e) => {
-              dispatch(updateEmailField(e.target.value))
-            }}
-          />
-        </div>
-        <div className="py-2">
-          <span className="px-1 text-sm text-gray-600">Password</span>
-          <input
-            placeholder=""
-            type="password"
-            x-model="password"
-            value={password}
-            className="text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none"
-            onChange={(e) => {
-              dispatch(updatePasswordField(e.target.value))
-            }}
-          />
-        </div>
+        <form onSubmit={formik.handleSubmit}>
+          <p className="font-medium text-center text-2xl mb-5 text-black">Login a profile</p>
 
-        <button
-          type="button"
-          className="mt-3 text-lg focus:outline-none font-semibold bg-gray-800 w-full text-white rounded-lg px-6 py-3 block shadow-xl hover:text-white hover:bg-black"
-          onClick={() => {
-            dispatch(loginFunction())
-          }}
-        >
-          Log in
-        </button>
+          <div className="py-2">
+            <div className="flex flex-row justify-between">
+              <span className="px-1 text-sm text-gray-600">Email</span>
+              {formik.errors.email && formik.touched.email && (
+                <p className="px-1 text-sm text-red-600">{formik.errors.email}</p>
+              )}
+            </div>
+            <input
+              placeholder=""
+              type="mail"
+              name="email"
+              value={formik.values.email}
+              x-model="password_confirm"
+              className="text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none"
+              onChange={formik.handleChange}
+            />
+          </div>
+
+          <div className="py-2">
+            <div className="flex flex-row justify-between">
+              <span className="px-1 text-sm text-gray-600">Password</span>
+              {formik.errors.password && formik.touched.password && (
+                <p className="px-1 text-sm text-red-600">{formik.errors.password}</p>
+              )}
+            </div>
+            <input
+              placeholder=""
+              type="password"
+              x-model="password"
+              name="password"
+              value={formik.values.password}
+              className="text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none"
+              onChange={formik.handleChange}
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="mt-3 text-lg focus:outline-none font-semibold bg-gray-800 w-full text-white rounded-lg px-6 py-3 block shadow-xl hover:text-white hover:bg-black"
+            // onClick={() => {
+            //   dispatch(loginFunction())
+            // }}
+          >
+            Log in
+          </button>
+        </form>
 
         <hr className="mb-2 mt-5 border-t border-teal-500" />
 
--- client/components/registration.js
+++ client/components/registration.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
+
 import {
   createUsernameField,
   createEmailField,
@@ -11,65 +14,121 @@ import Head from './head'
 
 const Registration = () => {
   const dispatch = useDispatch()
-  const username = useSelector((s) => s.reg.username)
-  const email = useSelector((s) => s.reg.email)
-  const password = useSelector((s) => s.reg.password)
+
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      email: '',
+      password: '',
+      confirm_password: ''
+    },
+    validationSchema: Yup.object({
+      username: Yup.string()
+        .min(4, 'Mininum 4 characters')
+        .max(12, 'Maximum 12 characters')
+        .required('Required!'),
+      email: Yup.string().email('Invalid email format').required('Required!'),
+      password: Yup.string().min(6, 'Minimum 6 characters').required('Required!'),
+      confirm_password: Yup.string()
+        .oneOf([Yup.ref('password')], "Password's not match")
+        .required('Required!')
+    }),
+    onSubmit: (values) => {
+      dispatch(createUsernameField(values.username))
+      dispatch(createEmailField(values.email))
+      dispatch(createPasswordField(values.password))
+      dispatch(createUser())
+    }
+  })
 
   return (
     <div className="h-screen w-screen">
       <Head title="Registration" />
       <div className="w-5/6 sm:w-3/5 md:w-2/4 lg:w-2/5 xl:w-1/4 2xl:w-1/4 px-6 flex justify-center flex-col  h-full mx-auto ">
-        <p className="font-medium text-center text-2xl mb-5 text-black">Create a profile</p>
-        <div className="py-2">
-          <span className="px-1 text-sm text-gray-600">Username</span>
-          <input
-            placeholder=""
-            type="text"
-            value={username}
-            x-model="password_confirm"
-            className="text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none"
-            onChange={(e) => {
-              dispatch(createUsernameField(e.target.value))
-            }}
-          />
-        </div>
-        <div className="py-2">
-          <span className="px-1 text-sm text-gray-600">Email</span>
-          <input
-            placeholder=""
-            type="email"
-            value={email}
-            x-model="password_confirm"
-            className="text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none"
-            onChange={(e) => {
-              dispatch(createEmailField(e.target.value))
-            }}
-          />
-        </div>
-        <div className="py-2">
-          <span className="px-1 text-sm text-gray-600">Password</span>
-          <input
-            placeholder=""
-            type="password"
-            x-model="password"
-            value={password}
-            className="text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none"
-            onChange={(e) => {
-              dispatch(createPasswordField(e.target.value))
-            }}
-          />
-        </div>
+        <form onSubmit={formik.handleSubmit}>
+          <p className="font-medium text-center text-2xl mb-5 text-black">Create a profile</p>
 
-        <button
-          type="button"
-          className="mt-3 text-lg focus:outline-none font-semibold bg-gray-800 w-full text-white rounded-lg px-6 py-3 block shadow-xl hover:text-white hover:bg-black"
-          onClick={() => {
-            dispatch(createUser())
-          }}
-        >
-          Log in
-        </button>
+          <div className="py-2">
+            <div className="flex flex-row justify-between">
+              <span className="px-1 text-sm text-gray-600">Username</span>
+              {formik.errors.username && formik.touched.username && (
+                <p className="px-1 text-sm text-red-600">{formik.errors.username}</p>
+              )}
+            </div>
+            <input
+              placeholder=""
+              type="text"
+              name="username"
+              value={formik.values.username}
+              x-model="password_confirm"
+              className="text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none"
+              onChange={formik.handleChange}
+            />
+          </div>
 
+          <div className="py-2">
+            <div className="flex flex-row justify-between">
+              <span className="px-1 text-sm text-gray-600">Email</span>
+              {formik.errors.email && formik.touched.email && (
+                <p className="px-1 text-sm text-red-600">{formik.errors.email}</p>
+              )}
+            </div>
+            <input
+              placeholder=""
+              type="mail"
+              name="email"
+              value={formik.values.email}
+              x-model="password_confirm"
+              className="text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none"
+              onChange={formik.handleChange}
+            />
+          </div>
+
+          <div className="py-2">
+            <div className="flex flex-row justify-between">
+              <span className="px-1 text-sm text-gray-600">Password</span>
+              {formik.errors.password && formik.touched.password && (
+                <p className="px-1 text-sm text-red-600">{formik.errors.password}</p>
+              )}
+            </div>
+
+            <input
+              placeholder=""
+              type="password"
+              x-model="password"
+              name="password"
+              value={formik.values.password}
+              className="text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none"
+              onChange={formik.handleChange}
+            />
+          </div>
+
+          <div className="py-2">
+            <div className="flex flex-row justify-between">
+              <span className="px-1 text-sm text-gray-600">Confirm Password</span>
+              {formik.errors.confirm_password && formik.touched.confirm_password && (
+                <p className="px-1 text-sm text-red-600">{formik.errors.confirm_password}</p>
+              )}
+            </div>
+
+            <input
+              placeholder=""
+              type="password"
+              x-model="confirm_password"
+              name="confirm_password"
+              value={formik.values.confirm_password}
+              className="text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none"
+              onChange={formik.handleChange}
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="mt-3 text-lg focus:outline-none font-semibold bg-gray-800 w-full text-white rounded-lg px-6 py-3 block shadow-xl hover:text-white hover:bg-black"
+          >
+            Register
+          </button>
+        </form>
         <hr className="mb-2 mt-5 border-t border-teal-500" />
 
         <div className="text-sm font-semibold block py-4 flex justify-center">
